07337f3a54db7849081af169b02c553bac2872d8
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2020-06-15 10:50:32 -0400

    Added a DTO class for case

---
+++ app/src/main/java/edu/uc/groupproject/covid19tracker/dto/Cases.kt
@@ -0,0 +1,3 @@
+package edu.uc.groupproject.covid19tracker.dto
+
+data class Cases(var activeCase: String, var fatalCase: String, var recoverdCase: String, var newCase: String, var newDeath: String) {}
