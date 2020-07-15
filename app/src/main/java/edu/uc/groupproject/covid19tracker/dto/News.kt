20347b78deb7a9d5e9ae944ddc20f3d4d31e80db
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2020-07-14 22:30:57 -0400

    retrieved news data

---
+++ app/src/main/java/edu/uc/groupproject/covid19tracker/dto/News.kt
@@ -0,0 +1,12 @@
+package edu.uc.groupproject.covid19tracker.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class News(@SerializedName("author") var author: ArrayList<String>,
+                @SerializedName("title") var title: ArrayList<String>,
+                @SerializedName("description") var description: ArrayList<String>,
+                @SerializedName("url") var url: ArrayList<String>,
+                @SerializedName("urlToImage") var urlToImage: ArrayList<String>,
+                @SerializedName("publishedAt") var publishedAt: ArrayList<String>,
+                @SerializedName("content") var content: ArrayList<String>) {
+}
